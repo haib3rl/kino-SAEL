9167f4fd7d598bf6aa5d08204b570b2c53b3fce0
haib3rl <lukas.haiber@mail.schwarz>
2021-10-30 11:32:57 +0200

    Added Index and a db.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # kino-SAEL
+## DB Integration
+1st Step: Open a browser and go to URL “http://localhost/phpmyadmin/”
+2nd Step: Then, click on the databases tab
+3rd Step: Create a database naming “cinema_db” and then click on the import tab
+4th Step: Click on browse file and select “cinema_db.sql” file which is inside the “database” folder
+5th Step: Click on go.
---
+++ Website/index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style/styles.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <title>CSS Kino</title>
+    <link rel="icon" type="image/png" href="img/logo.png">
+</head>
+ 
+<body>
+    <?php
+    $link = mysqli_connect("localhost", "root", "", "cinema_db");
+    $sql = "SELECT * FROM movieTable";
+    ?>
+    <header></header>
+    <div id="home-section-1" class="movie-show-container">
+        <h1>Aktuell gespielt</h1>
+        <h3>jetzt Tickets buchen</h3>
+
+        <div class="movies-container">
+
+            <?php
+                        if($result = mysqli_query($link, $sql)){
+                            if(mysqli_num_rows($result) > 0){
+                                for ($i = 0; $i <= 5; $i++){
+                                    $row = mysqli_fetch_array($result);
+                                    echo '<div class="movie-box">';
+                                    echo '<img src="'. $row['movieImg'] .'" alt=" ">';
+                                    echo '<div class="movie-info ">';
+                                    echo '<h3>'. $row['movieTitle'] .'</h3>';
+                                    echo '<p> FSK:'. $row['movieAgeLevel'] . '</p>';
+                                    echo '<a href="booking.php?id='.$row['movieID'].'"><i class="fas fa-ticket-alt"></i> Details</a>';
+                                    echo '</div>';
+                                    echo '</div>';
+                                }
+                                mysqli_free_result($result);
+                            } else{
+                                echo '<h4 class="no-annot">No Booking to our movies right now</h4>';
+                            }
+                        } else{
+                            echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+                        }
+                        
+                        // Close connection
+                        mysqli_close($link);
+                        ?>
+        </div>
+    </div>
+    <div id="home-section-2" class="services-section">
+        <h1>Wie es funktioniert</h1>
+        <h3>3 Einfache Schritte bis zum Ticket</h3>
+
+        <div class="services-container">
+            <div class="service-item">
+                <div class="service-item-icon">
+                    <i class="fas fa-4x fa-video"></i>
+                </div>
+                <h2>1. Film auswählen</h2>
+                <p>Wählen sie den zu schauenden Film aus</p>
+            </div>
+            <div class="service-item">
+                <div class="service-item-icon">
+                    <i class="fas fa-4x fa-credit-card"></i>
+                </div>
+                <h2>2. Wählen sie die Vorstellung und einen Sitz aus</h2>
+                <p></p>
+            </div>
+            <div class="service-item">
+                <div class="service-item-icon">
+                    <i class="fas fa-4x fa-theater-masks"></i>
+                </div>
+                <h2>Kontaktdaten eingeben</h2>
+                <p>choose the movie you want to watch at our theaters</p>
+            </div>
+            <div class="service-item"></div>
+            <div class="service-item"></div>
+        </div>
+    </div>
+    <footer></footer>
+
+
+</body>
+
+</html>
